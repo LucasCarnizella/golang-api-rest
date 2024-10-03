33a74a4dd4f958642945f1fc8c5a61a3c5520cfd
LucasCarnizella <lucascarnizella035@hotmail.com>
2024-10-02 22:46:24 -0300

    feat: add update and delete methods in the personas CRUD and rename functions to better met resemble the methods

--- controllers/personas.go
+++ controllers/personas.go
@@ -9,25 +9,89 @@ import (
 	"net/http"
 )
 
-func GetAllPersonas(w http.ResponseWriter, r *http.Request) {
+func ReadAllPersonas(w http.ResponseWriter, r *http.Request) {
 	var personas []models.Persona
 
 	database.DB.Find(&personas)
+
 	err := json.NewEncoder(w).Encode(personas)
 	if err != nil {
 		log.Panic(err)
 	}
 }
 
-func GetPersona(w http.ResponseWriter, r *http.Request) {
+func CreatePersona(w http.ResponseWriter, r *http.Request) {
+	var persona models.Persona
+	var err error
+
+	err = json.NewDecoder(r.Body).Decode(&persona)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if persona.Name == "" || persona.Story == "" {
+		log.Panic("One of the variables is an empty string.")
+	}
+
+	database.DB.Create(&persona)
+
+	err = json.NewEncoder(w).Encode(persona)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func ReadPersona(w http.ResponseWriter, r *http.Request) {
+	var persona models.Persona
+
 	vars := mux.Vars(r)
 	id := vars["id"]
-
-	var persona models.Persona
 
 	database.DB.First(&persona, id)
 	err := json.NewEncoder(w).Encode(persona)
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func UpdatePersona(w http.ResponseWriter, r *http.Request) {
+	var persona models.Persona
+	var err error
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	database.DB.First(&persona, id)
+
+	err = json.NewDecoder(r.Body).Decode(&persona)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if persona.Name == "" || persona.Story == "" {
+		log.Panic("One of the variables is an empty string.")
+	}
+
+	database.DB.Save(&persona)
+
+	err = json.NewEncoder(w).Encode(&persona)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func DeletePersona(w http.ResponseWriter, r *http.Request) {
+	var persona models.Persona
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	database.DB.First(&persona, id)
+
+	err := json.NewEncoder(w).Encode(persona)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	database.DB.Delete(&persona, id)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -11,8 +11,11 @@ func HandleRequest(port string) {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/ping", controllers.Ping)
-	r.HandleFunc("/api/v1/personas", controllers.GetAllPersonas).Methods("Get")
-	r.HandleFunc("/api/v1/personas/{id}", controllers.GetPersona).Methods("Get")
+	r.HandleFunc("/api/v1/personas", controllers.ReadAllPersonas).Methods("Get")
+	r.HandleFunc("/api/v1/personas", controllers.CreatePersona).Methods("Post")
+	r.HandleFunc("/api/v1/personas/{id}", controllers.ReadPersona).Methods("Get")
+	r.HandleFunc("/api/v1/personas/{id}", controllers.UpdatePersona).Methods("Put")
+	r.HandleFunc("/api/v1/personas/{id}", controllers.DeletePersona).Methods("Delete")
 
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
