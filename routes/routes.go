bab2984bda42fb121421e67e21d5976ce29bd45a
LucasCarnizella <lucascarnizella035@hotmail.com>
2024-10-06 21:54:23 -0300

    feat: add middleware to handle content type

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"github.com/LucasCarnizella/golang-api-rest/controllers"
+	"github.com/LucasCarnizella/golang-api-rest/middlewares"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -9,6 +11,9 @@ import (
 
 func HandleRequest(port string) {
 	r := mux.NewRouter()
+
+	r.Use(middlewares.ContentTypeMiddleware)
+
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/ping", controllers.Ping)
 	r.HandleFunc("/api/v1/personas", controllers.ReadAllPersonas).Methods("Get")
@@ -17,5 +22,5 @@ func HandleRequest(port string) {
 	r.HandleFunc("/api/v1/personas/{id}", controllers.UpdatePersona).Methods("Put")
 	r.HandleFunc("/api/v1/personas/{id}", controllers.DeletePersona).Methods("Delete")
 
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
